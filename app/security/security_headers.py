b221c7fcb3d14d3310008256399fc5be12af6258
YUSRI ADIB <muhdyusriadib@gmail.com>
2025-09-03 00:41:48 +0800

    Refactor SecurityHeadersMiddleware and update headers
    Removed unnecessary constructor and added X-XSS-Protection header.

--- app/security/security_headers.py
+++ app/security/security_headers.py
@@ -1,15 +1,9 @@
 from starlette.middleware.base import BaseHTTPMiddleware
-from starlette.types import ASGIApp, Receive, Scope, Send
 
 class SecurityHeadersMiddleware(BaseHTTPMiddleware):
-    def __init__(self, app: ASGIApp):
-        super().__init__(app)
-
     async def dispatch(self, request, call_next):
         response = await call_next(request)
-        response.headers["Content-Security-Policy"] = "default-src 'self'; img-src 'self' data:;"
-        response.headers["Strict-Transport-Security"] = "max-age=31536000; includeSubDomains"
-        response.headers["X-Content-Type-Options"] = "nosniff"
-        response.headers["X-Frame-Options"] = "DENY"
-        response.headers["Referrer-Policy"] = "no-referrer"
+        response.headers['X-Content-Type-Options'] = 'nosniff'
+        response.headers['X-Frame-Options'] = 'DENY'
+        response.headers['X-XSS-Protection'] = '1; mode=block'
         return response
